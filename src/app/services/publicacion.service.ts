33a252f7ce20930bedb32214e889bfaa283a6c64
David Martin <davidskisnot@gmail.com>
2024-04-29 16:44:00 +0200

    arreglo filtro

--- src/app/services/publicacion.service.ts
+++ src/app/services/publicacion.service.ts
@@ -8,7 +8,7 @@ import { Observable, catchError, throwError } from 'rxjs';
 })
 export class PublicacionService {
 
-  private baseUrl = 'https://dbateofront.vercel.app/'; // URL base de tu backend
+  private baseUrl = 'https://dbateofront.vercel.app'; // URL base de tu backend
 
   constructor(private http: HttpClient) { }
 

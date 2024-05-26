f72d7bd22f9717277bac59d96a7a5906cdbf9adf
yodavidm <davidskisnot@gmail.com>
2024-05-26 04:02:22 +0200

    test id

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Publicacion } from 'src/app/interfaces/publicacion';
 import { SeguidorDTO } from 'src/app/interfaces/seguidor';
 import { Usuario } from 'src/app/interfaces/usuario';
@@ -16,30 +15,32 @@ import Swal from 'sweetalert2';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private publicacionService: PublicacionService, private route: ActivatedRoute, private router: Router, private authService: AuthService, private usuarioService: UsuarioService) { }
+  constructor(
+    private publicacionService: PublicacionService, 
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private authService: AuthService, 
+    private usuarioService: UsuarioService
+  ) { }
 
   publicaciones!: Publicacion[];
   public page!: number;
-  usuario!: Usuario
+  usuario!: Usuario;
   esAdmin!: boolean;
   idUsuario!: number;
+  hayPublicacionesCategoria!: boolean;
+  seguidor: SeguidorDTO = { idSeguidor: 0, idSeguido: 0 };
 
   ngOnInit(): void {
-
-
     this.getUserId();
     this.cargarUsuario();
-
     this.isAdmin();
 
     this.route.params.subscribe(params => {
       const idCategoria = +params['categoria']; // Convertir a número
-
       if (!isNaN(idCategoria)) {
-        // Si hay un id de categoría en la url, filtrar por esa categoría
         this.obtenerPublicacionesPorCategoria(idCategoria);
       } else {
-        // Si no hay id de categoría en la url, obtener todas las publicaciones
         this.obtenerPublicaciones();
       }
     });
@@ -52,7 +53,6 @@ export class MainComponent implements OnInit {
   obtenerPublicacionesPorCategoria(idCategoria: number): void {
     this.publicacionService.obtenerPublicacionesPorCategoria(idCategoria).subscribe(
       publicaciones => {
-        // Ordenar las publicaciones por fecha_creacion de más reciente a más antigua
         this.publicaciones = publicaciones.sort((a, b) => {
           return new Date(b.fecha_creacion).getTime() - new Date(a.fecha_creacion).getTime();
         });
@@ -63,24 +63,19 @@ export class MainComponent implements OnInit {
     );
   }
 
-
-  hayPublicacionesCategoria!: boolean;
-
-
   obtenerPublicaciones(): void {
     this.publicacionService.obtenerPublicaciones()
       .subscribe(
         publicaciones => {
-          // Ordenar las publicaciones por fecha de creación de forma descendente
           this.publicaciones = publicaciones.sort((a, b) => {
             return new Date(b.fecha_creacion).getTime() - new Date(a.fecha_creacion).getTime();
           });
-  
+
           // Verificar seguimiento para cada publicación
           this.publicaciones.forEach(publicacion => {
             this.verificarSeguimiento(publicacion);
           });
-  
+
           console.log('Publicaciones obtenidas:', this.publicaciones);
         },
         error => {
@@ -88,7 +83,6 @@ export class MainComponent implements OnInit {
         }
       );
   }
-  
 
   cambiarPagina(nuevaPagina: number): void {
     this.router.navigate([], {
@@ -102,9 +96,7 @@ export class MainComponent implements OnInit {
     this.page = 1;
   }
 
-
   async eliminarPublicacion(id: number): Promise<void> {
-    // Mostrar un cuadro de diálogo de confirmación con SweetAlert
     const result = await Swal.fire({
       title: '¿Estás seguro?',
       text: "Esta acción eliminará la publicación y no se podrá deshacer más tarde.",
@@ -116,15 +108,11 @@ export class MainComponent implements OnInit {
       cancelButtonText: 'Cancelar'
     });
 
-    // Verificar la opción seleccionada por el usuario
     if (result.isConfirmed) {
-      // Llamar al servicio para eliminar la publicación
       this.publicacionService.eliminarPublicacion(id)
         .subscribe(
           () => {
             console.log(`Publicación con ID ${id} eliminada correctamente`);
-
-            // Recargar la página después de eliminar la publicación
             window.location.reload();
           },
           error => {
@@ -132,12 +120,10 @@ export class MainComponent implements OnInit {
           }
         );
     } else {
-      // El usuario ha cancelado la acción
       console.log('La acción ha sido cancelada');
     }
   }
 
-  //mandar id de publicacion al pulsar en comentarios
   verComentarios(idPublicacion: number): void {
     this.router.navigate(['/publicacion', idPublicacion, 'comentarios']);
   }
@@ -146,17 +132,13 @@ export class MainComponent implements OnInit {
     this.router.navigate(['publicacion', idPublicacion, 'comentar']);
   }
 
-
-  //comprobar que es la publicacion del usuario logueado
   esMiPublicacionPropia(publicacion: Publicacion): boolean {
     const userId = this.authService.getUserId();
-
     if (userId !== null) {
       const idUsuario = parseInt(userId);
       return publicacion.usuario.id === idUsuario;
     }
     return false;
-
   }
 
   isAdmin(): void {
@@ -166,11 +148,7 @@ export class MainComponent implements OnInit {
       this.authService.getUserById(idUsuario).subscribe({
         next: (usuario: Usuario) => {
           this.usuario = usuario;
-          if (usuario.rol.nombreRol === 'ADMIN_ROLE') {
-            this.esAdmin = true;
-          } else {
-            this.esAdmin = false;
-          }
+          this.esAdmin = usuario.rol.nombreRol === 'ADMIN_ROLE';
         },
         error: (error) => {
           console.error('Error al obtener el usuario', error);
@@ -179,15 +157,7 @@ export class MainComponent implements OnInit {
     }
   }
 
-  //logica para seguir a otro usuario
-
-  seguidor: SeguidorDTO = {
-    idSeguidor: 0,
-    idSeguido: 0
-  }
-
   seguir(seguidor: SeguidorDTO, publicacion: Publicacion) {
-
     const userId = this.authService.getUserId();
     const seguidoId = publicacion.usuario.id;
     if (userId != null) {
@@ -195,21 +165,20 @@ export class MainComponent implements OnInit {
       seguidor.idSeguidor = seguidorId;
       seguidor.idSeguido = seguidoId;
 
-
       this.usuarioService.seguirUsuario(seguidor).subscribe({
         next: (data) => {
           location.reload();
           console.log("Usuario seguido correctamente");
-
         },
         error: (error) => {
           console.log("Hubo un error al seguir ids: " + seguidor.idSeguido + " " + seguidor.idSeguidor);
         }
-      })
+      });
     }
   }
-  
+
   dejarDeSeguir(publicacion: Publicacion): void {
+    if (!this.usuario) return;
     this.usuarioService.dejarDeSeguir(this.usuario.id, publicacion.usuario.id).subscribe(
       () => {
         console.log(`Has dejado de seguir al usuario con ID: ${publicacion.usuario.id}`);
@@ -220,29 +189,20 @@ export class MainComponent implements OnInit {
       }
     );
   }
-  
-  
-  
 
   verificarSeguimiento(publicacion: Publicacion): void {
+    if (!this.usuario) return;
     this.usuarioService.verificarSeguimientoExistente(this.usuario.id, publicacion.usuario.id)
       .subscribe(
         resultado => {
-          if (resultado) {
-            publicacion.estaSiguiendo = true;
-          } else {
-            publicacion.estaSiguiendo = false;
-          }
+          publicacion.estaSiguiendo = resultado;
         },
         error => {
           console.error('Error al verificar seguimiento:', error);
         }
       );
   }
-  
 
-
-  //cargar usuario actual
   getUserId(): void {
     const userId = this.authService.getUserId();
     if (userId !== null) {
@@ -258,7 +218,6 @@ export class MainComponent implements OnInit {
         (data: Usuario) => {
           this.usuario = data;
           console.log(this.usuario.nickname);
-          
         },
         (error: any) => {
           console.error("Error al obtener los datos de usuario", error);
@@ -266,6 +225,5 @@ export class MainComponent implements OnInit {
       );
     }
   }
-
 
 }
